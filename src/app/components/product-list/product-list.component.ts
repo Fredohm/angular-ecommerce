0c9c51cb1192fd8eae4edf577824c6ae7e340c57
Fredohm <fredwaucquez@hotmail.fr>
2021-05-16 19:24:57 +0200

    Search for Products by category. Add dynamic search component

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  currentCategoryName: string;
 
   constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
@@ -32,10 +33,14 @@ export class ProductListComponent implements OnInit {
     if (hasCategoryId) {
       // get the "id" param and convert to number with a +
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+
+      // get the "name" param
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name');
     }
     else {
       // default to 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     // get products for the given category id

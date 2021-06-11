859892dc594fd5fd4883f90da772b6c8360b97ba
Fredohm <fredwaucquez@hotmail.fr>
2021-06-11 11:39:10 +0200

    Add BehaviorSubject observer for review order.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -4,6 +4,7 @@ import {Luv2ShopFormService} from '../../services/luv2-shop-form.service';
 import {Country} from '../../common/country';
 import {State} from '../../common/state';
 import {Luv2ShopValidators} from '../../validators/luv2-shop-validators';
+import {CartService} from '../../services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -26,9 +27,12 @@ export class CheckoutComponent implements OnInit {
   billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder,
-              private luv2ShopFormService: Luv2ShopFormService) { }
+              private luv2ShopFormService: Luv2ShopFormService,
+              private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.reviewCartDetails();
+
     this.checkOutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('',
@@ -228,6 +232,17 @@ export class CheckoutComponent implements OnInit {
         // select first item as default
         formGroup.get('state').setValue(data[0]);
       }
+    );
+  }
+
+  // tslint:disable-next-line:typedef
+  private reviewCartDetails() {
+    // Subscribe to cartService for totals
+    this.cartService.totalQuantity.subscribe(
+      totalQuantity => this.totalQuantity = totalQuantity
+    );
+    this.cartService.totalPrice.subscribe(
+      totalPrice => this.totalPrice = totalPrice
     );
   }
 

caf29c6616e2b5e98218b3cee15283d59debb2c6
Fredohm <fredwaucquez@hotmail.fr>
2021-06-15 10:58:08 +0200

    Add login and login status components. App failure if no remove line 30 in angular.json

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,6 +1,6 @@
 export default {
   oidc: {
-    clientId: '0oa104e8q2KOaerel5d7',
+    clientId: '0oa1096478VHiXTlt5d7',
     issuer: 'https://dev-55181450.okta.com/oauth2/default',
     redirectUri: 'http://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']

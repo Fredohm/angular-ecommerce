9aed48e5a978a1dc8a6d5c3fefd281951e5a03ba
Fredohm <fredwaucquez@hotmail.fr>
2021-06-17 06:43:48 +0200

    Order History - Secure Orders API frontend - Add Interceptor for token

---
+++ src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {from, Observable} from 'rxjs';
+import {OktaAuthService} from '@okta/okta-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor(private oktaAuth: OktaAuthService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return from(this.handleAccess(request, next));
+  }
+
+  // tslint:disable-next-line:typedef
+  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    // Only add access token for secured endpoints
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
+
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+      // get access token
+      const accessToken = await this.oktaAuth.getAccessToken();
+
+      // clone request for adding new header with token
+      request = request.clone({
+          setHeaders: {
+            Authorisation: 'Bearer' + accessToken
+          }
+      });
+      return next.handle(request).toPromise();
+    }
+  }
+}
